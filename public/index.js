c65159d2abc68e4c770e87d6213080f88e8d0137
Chuck Danielsson <chuck.danielsson@weareadaptive.com>
2022-01-21 13:01:07 -0500

    using the same manifest for platforms

--- public/index.js
+++ public/index.js
@@ -1,4 +1,5 @@
 const launchPlatformButton = document.getElementById('launch-platform-app')
+const launchPlatformButton2 = document.getElementById('launch-platform-app-two')
 const launchAppOntoPlatformButton = document.getElementById('launch-app-onto-platform')
 const launchOtherPlatformButton = document.getElementById('launch-other-platform-app')
 const middlewareLauncherButton = document.getElementById('middleware-app')
@@ -11,7 +12,16 @@ const launchPlatforButtonOnClick = async () => {
     }
 }
 
+const launchPlatformSecondButtonOnClick = async () => {
+    try {
+        fin.Application.startFromManifest(`${window.location.origin}/platform_app_new.json`)
+    } catch (e) {
+        console.error('Error in startFromManifest', e)
+    }
+}
+
 launchPlatformButton.onclick = launchPlatforButtonOnClick
+launchPlatformButton2.onclick = launchPlatformSecondButtonOnClick
 
 // This is the target platform UUID - it is in the platform_app.json
 const TARGET_PLATFORM_UUDI = 'example_platform'
@@ -34,3 +44,14 @@ const launchMiddleware = () => {
 }
 
 middlewareLauncherButton.onclick = launchMiddleware
+
+const platformStatusHeadline = document.getElementById('platform-status')
+
+const onLoad = () => {
+    if (fin.me.isView) {
+        platformStatusHeadline.innerText = 'Hello From Platform'
+    } else {
+        platformStatusHeadline.innerText = 'Hello From A Window'
+    }
+}
+onLoad()
